3cb1524247f449ecb00fa4c1f3a6399cac0ef748
avanza <avanza@10.0.2.15>
2014-05-12 12:16:32 +0100

    version 1.1

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -15,5 +15,7 @@ public class Calculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("esto es una prueba");
     }
 }
